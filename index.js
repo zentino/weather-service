fe321051526c1eba1628f4d27e3475fd7f124267
zentino <sn-z777@outlook.com>
2019-05-17 19:15:18 +0200

    Add implementation in getAvailableCities and haversine formula

--- index.js
+++ index.js
@@ -19,19 +19,19 @@ server.pre((req, res, next) => {
 
 server.get('/cities/:city_id', (req, res, next) => {
   let city = controller.getCityById(req.params.city_id);
-    if(city) {
-      city["lat"] = city.coord.lat;
-      city["lng"] = city.coord.lon;
-      delete city["coord"];
-      delete city["country"];
-      res.json(200, city);
-    } else {
-      res.json(404, {
-        code:"NotFoundError",
-        message:"not found"
-      });
-    }
-    return next();
+  if(city) {
+    city["lat"] = city.coord.lat;
+    city["lng"] = city.coord.lon;
+    delete city["coord"];
+    delete city["country"];
+    res.json(200, city);
+  } else {
+    res.json(404, {
+      code:"NotFoundError",
+      message:"not found"
+    });
+  }
+  return next();
 });
 
 server.get('/cities/:city_id/weather', (req, res, next) => {
@@ -43,8 +43,15 @@ server.get('/cities/:city_id/weather', (req, res, next) => {
 server.get('/cities', (req, res, next) => {
   const lat = req.query.lat;
   const lng = req.query.lng;
-  controller.getAvailableCities(lat, lng);
-  res.send(200);
+  if(lat && lng) {
+    const cities = controller.getAvailableCities(lat, lng);
+    res.json(200, cities);
+  } else {
+    res.json(400, {
+      code:"BadRequestError",
+      message:"lat/lng required"
+    });
+  }
   return next();
 });
 
--- weather.controller.js
+++ weather.controller.js
@@ -2,6 +2,7 @@
 const fs = require('fs');
 const rawdata = fs.readFileSync('city.list.json');
 const cityList = JSON.parse(rawdata);
+const RADIUS = 10;
 
 class WeatherController {
 
@@ -15,7 +16,34 @@ class WeatherController {
   }
 
   getAvailableCities(latitude, longitude) {
-    console.info(`getAvailableCities latitude = ${latitude}, longitude = ${longitude}`);
+    let lat1 = parseFloat(latitude);
+    let lon1 = parseFloat(longitude);
+    let cities = [];
+    cityList.filter(it => {
+        if(this.calculateDistance(parseFloat(latitude), parseFloat(longitude), it.coord.lat, it.coord.lon) <= RADIUS) {
+          cities.push({id:it.id, name:it.name});
+        }
+      }
+    );
+    return cities;
+  }
+
+  // Haversine formula to calculate the distance between two points
+  calculateDistance(lat1, lon1, lat2, lon2) {
+    let R = 6371 // kilometres
+    let pi = Math.PI;
+    let φ1 = lat1 * (pi/180);
+    let φ2 = lat2 * (pi/180);
+    let Δφ = (lat2-lat1) * (pi/180);
+    let Δλ = (lon2-lon1) * (pi/180);
+
+    let a = Math.sin(Δφ/2) * Math.sin(Δφ/2) +
+            Math.cos(φ1) * Math.cos(φ2) *
+            Math.sin(Δλ/2) * Math.sin(Δλ/2);
+    let c = 2 * Math.atan2(Math.sqrt(a), Math.sqrt(1-a));
+
+    let d = R * c;
+    return d;
   }
 
 }
